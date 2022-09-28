d506df03044ba9d5b3d3be46917dbd0577b4b4d3
hanmariyang <hanmari.code@gmail.com>
2022-09-28 09:26:02 +0900

    day-03 django

--- pop_test/settings.py
+++ pop_test/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 내가 생성한 앱
     'introduce',
     'pop_test',
+    'customuser',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'customuser.CustomModel'
